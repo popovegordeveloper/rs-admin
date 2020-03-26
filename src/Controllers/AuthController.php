385b546e4791f2a07b84601d12700b5b4069bd00
Egor Popov <egor.popov@rocketstation.ru>
2020-03-26 17:58:12 +0300

    fix

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -15,6 +15,8 @@ class AuthController extends Controller
         if ($request->getMethod() == 'GET')
             return view('rs_admin::login.index');
 
-        if (Auth::guard()->attempt($request->only(['email', 'password'])));
+        if (Auth::guard()->attempt($request->only(['email', 'password']))){
+
+        }
     }
 }

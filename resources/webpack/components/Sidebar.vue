625be16aece55d98f2e1354bd199abc4291b416d
Egor Popov <egor.popov@rocketstation.ru>
2020-04-07 17:28:44 +0300

    sidebar

--- resources/webpack/components/Sidebar.vue
+++ resources/webpack/components/Sidebar.vue
@@ -11,6 +11,16 @@
                 </span>
             </p>
         </div>
+        <div class="content-sidebar">
+            <ul class="nav-main">
+                <li>
+                    <a href="/admin">
+                        <i class="fal fa-browser menu-item-icon"></i>
+                        <span>Главная</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </nav>
 </template>
 
@@ -38,6 +48,31 @@
         height: 100vh
         box-shadow: 5px 0 10px #e4e7ed
 
+        .content-sidebar
+            padding: 18px
+
+            .nav-main
+                padding: 0
+                li
+                    list-style: none
+                    a
+                        color: rgba(87,87,87,.9)
+                        text-decoration: none
+                        font-weight: 600
+                        font-size: 14px
+                        padding: 9px 18px 9px 0px
+                        transition: color .12s ease-out
+                        .menu-item-icon
+                            margin-right: 10px
+                        span
+                            transition: all .26s cubic-bezier(.47,0,.745,.715)
+                            letter-spacing: 0.5px
+
+                        &:hover
+                            span
+                                color: #000
+                            .menu-item-icon
+                                color: $primary-color
         .btn-logout
             margin-left: 10px
             cursor: pointer
@@ -51,6 +86,7 @@
             padding-top: 20px
             padding-bottom: 20px
             background-color: #f6f7f9
+            font-size: .9rem
 
         .logo
             width: 100%
